3a1bb502eaad1ee3493c7f1ceaa87b4f85236179
xun <2042204285@qq.com>
2022-10-17 15:36:06 +0800

    moment

---
+++ src/helpers/url.ts
@@ -0,0 +1,54 @@
+import { isDate, isObject } from './util'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export default function buildUrl(url: string, params?: any): string {
+  // 如果为空直接返回
+  if (!params) {
+    return url
+  }
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+    let values = []
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    const marIndex = url.indexOf('#')
+
+    if (marIndex !== -1) {
+      url = url.slice(0, marIndex)
+    }
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+  return url
+}
