be8b12dc9b713229b18a78a9011cdd8dd018057a
xun <2042204285@qq.com>
2022-10-17 11:36:46 +0800

    axios初始化

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'patch'
+  | 'PATCH'
+  | 'put'
+  | 'PUT'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
