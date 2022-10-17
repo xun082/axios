b4f3c8241290347b07dd4bd3e7ac12e40fae5527
xun <2042204285@qq.com>
2022-10-17 16:25:07 +0800

    moment

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import buildUrl from './helpers/url'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 export default function axios(config: AxiosRequestConfig) {
   processConfig(config)
@@ -10,6 +11,7 @@ export default function axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
@@ -20,4 +22,9 @@ function transformUrl(config: AxiosRequestConfig): string {
 
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
