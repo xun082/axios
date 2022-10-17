30960e7d6f7c969f1c019650a5198dacc1c6707c
xun <2042204285@qq.com>
2022-10-18 07:39:09 +0800

    moment

--- src/xhr.ts
+++ src/xhr.ts
@@ -3,12 +3,16 @@ import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
     const request = new XMLHttpRequest()
 
     if (responseType) {
       request.responseType = responseType
+    }
+
+    if (timeout) {
+      request.timeout = timeout
     }
 
     request.open(method.toUpperCase(), url, true)
@@ -16,6 +20,14 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
         return
+      }
+
+      request.onerror = function handleError() {
+        reject(new Error('NetWork Error'))
+      }
+
+      request.ontimeout = function handleTimeout() {
+        reject(new Error(`timeout of ${timeout} ms exceeded`))
       }
 
       const responseHeaders = parseHeaders(request.getAllResponseHeaders())
