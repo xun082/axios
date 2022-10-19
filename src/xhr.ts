4f492e8b564e31725194050da55fc3aad847d614
xun <2042204285@qq.com>
2022-10-20 07:18:10 +0800

    moment

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 
 import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
@@ -23,11 +24,11 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (request.status === 0) return
 
       request.onerror = function handleError() {
-        reject(new Error('NetWork Error'))
+        reject(createError('NetWork Error', config, null, request))
       }
 
       request.ontimeout = function handleTimeout() {
-        reject(new Error(`timeout of ${timeout} ms exceeded`))
+        reject(createError(`timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
       }
 
       const responseHeaders = parseHeaders(request.getAllResponseHeaders())
@@ -58,7 +59,15 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (response.status >= 200 && response.status < 300) {
         resolve(response)
       } else {
-        reject(new Error(`Request failed with status code ${response.status}`))
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
       }
     }
   })
