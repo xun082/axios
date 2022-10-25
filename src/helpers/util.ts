e07d54464e759d21bea5f88a95a93da481f76cf7
xun <2042204285@qq.com>
2022-10-25 17:06:03 +0800

    moment

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -4,10 +4,6 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-// export function isObject(val: any): val is Object {
-//   return val !== null && typeof val === 'object'
-// }
-
 export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
 }
@@ -21,4 +17,8 @@ export function extend<T, U>(to: T, from: U): T & U {
     ;(to as T & U)[key] = from[key] as any
   }
   return to as T & U
+}
+
+export function isURLSearchParams(value: any): value is URLSearchParams {
+  return typeof value !== 'undefined' && value instanceof URLSearchParams
 }
